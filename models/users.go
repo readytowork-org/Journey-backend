938afc8977707b130af318d39b665cac4f160c6e
saurav <saurav.maharjan7@gmail.com>
2022-08-25 17:16:38 +0545

    created model for users

--- models/users.go
+++ models/users.go
@@ -2,8 +2,16 @@ package models
 
 type User struct {
 	Base
-	Email          string `json:"email"`
-	FullName       string `json:"full_name"`
+	Id         int    `json:"id"`
+	Email      string `json:"email"`
+	FullName   string `json:"full_name"`
+	CreatedAt  string `json:"created_at"`
+	UpdatedAt  string `json:"updated_at"`
+	DeletedAt  string `json:"deleted_at"`
+	ProfileUrl string `json:"profile_url"`
+	Bio        string `json:"bio"`
+	CoverUrl   string `json:"cover_url"`
+	IsCreator  bool   `json:"is_creator"`
 }
 
 // TableName gives table name of model
@@ -14,7 +22,15 @@ func (m User) TableName() string {
 // ToMap convert User to map
 func (m User) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"email":           m.Email,
-		"full_name":       m.FullName,
+		"id":          m.Id,
+		"email":       m.Email,
+		"full_name":   m.FullName,
+		"created_at":  m.CreatedAt,
+		"updated_at":  m.UpdatedAt,
+		"deleted_at":  m.DeletedAt,
+		"profile_url": m.ProfileUrl,
+		"bio":         m.Bio,
+		"cover_url":   m.CoverUrl,
+		"is_creator":  m.IsCreator,
 	}
 }
