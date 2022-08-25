30b922731e2c0f65ef353109aab94a01affb5121
saurav <saurav.maharjan7@gmail.com>
2022-08-25 18:13:13 +0545

    removed Id field

--- models/users.go
+++ models/users.go
@@ -1,17 +1,20 @@
 package models
 
+import (
+	"time"
+)
+
 type User struct {
 	Base
-	Id         int    `json:"id"`
-	Email      string `json:"email"`
-	FullName   string `json:"full_name"`
-	CreatedAt  string `json:"created_at"`
-	UpdatedAt  string `json:"updated_at"`
-	DeletedAt  string `json:"deleted_at"`
-	ProfileUrl string `json:"profile_url"`
-	Bio        string `json:"bio"`
-	CoverUrl   string `json:"cover_url"`
-	IsCreator  bool   `json:"is_creator"`
+	Email      string    `json:"email"`
+	FullName   string    `json:"full_name"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	DeletedAt  time.Time `json:"deleted_at"`
+	ProfileUrl string    `json:"profile_url"`
+	Bio        string    `json:"bio"`
+	CoverUrl   string    `json:"cover_url"`
+	IsCreator  bool      `json:"is_creator"`
 }
 
 // TableName gives table name of model
@@ -22,7 +25,6 @@ func (m User) TableName() string {
 // ToMap convert User to map
 func (m User) ToMap() map[string]interface{} {
 	return map[string]interface{}{
-		"id":          m.Id,
 		"email":       m.Email,
 		"full_name":   m.FullName,
 		"created_at":  m.CreatedAt,
