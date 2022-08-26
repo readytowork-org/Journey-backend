0883673c8ed5959640cbfc11856ee3fea020a593
saurav <saurav.maharjan7@gmail.com>
2022-08-26 15:12:44 +0545

    created posts route

---
+++ api/routes/posts.go
@@ -0,0 +1,44 @@
+package routes
+
+import (
+	"boilerplate-api/api/controllers"
+	"boilerplate-api/api/middlewares"
+	"boilerplate-api/infrastructure"
+)
+
+// PostRoutes -> struct
+type PostRoutes struct {
+	logger         infrastructure.Logger
+	router         infrastructure.Router
+	PostController controllers.PostsController
+	middleware     middlewares.FirebaseAuthMiddleware
+	trxMiddleware  middlewares.DBTransactionMiddleware
+}
+
+// Setup Post routes
+func (i PostRoutes) Setup() {
+	i.logger.Zap.Info(" Setting up Post routes")
+	Posts := i.router.Gin.Group("/Posts")
+	{
+		Posts.GET("", i.PostController.GetAllPosts)
+		Posts.POST("", i.trxMiddleware.DBTransactionHandle(), i.PostController.CreatePosts)
+		Posts.DELETE("/:id", i.PostController.DeletePosts)
+	}
+}
+
+// NewPostRoutes -> creates new Post controller
+func NewPostRoutes(
+	logger infrastructure.Logger,
+	router infrastructure.Router,
+	PostController controllers.PostsController,
+	middleware middlewares.FirebaseAuthMiddleware,
+	trxMiddleware middlewares.DBTransactionMiddleware,
+) PostRoutes {
+	return PostRoutes{
+		router:         router,
+		logger:         logger,
+		PostController: PostController,
+		middleware:     middleware,
+		trxMiddleware:  trxMiddleware,
+	}
+}
