36bf60efbe571d16cc9fad7b066d2297e27dcd4b
saurav <saurav.maharjan7@gmail.com>
2022-08-26 12:09:38 +0545

    update on fields using base

--- models/users.go
+++ models/users.go
@@ -1,20 +1,16 @@
 package models
 
-import (
-	"time"
-)
-
 type User struct {
 	Base
-	Email      string    `json:"email"`
-	FullName   string    `json:"full_name"`
-	CreatedAt  time.Time `json:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at"`
-	DeletedAt  time.Time `json:"deleted_at"`
-	ProfileUrl string    `json:"profile_url"`
-	Bio        string    `json:"bio"`
-	CoverUrl   string    `json:"cover_url"`
-	IsCreator  bool      `json:"is_creator"`
+	Email    string `json:"email"`
+	FullName string `json:"full_name"`
+	// CreatedAt  time.Time `json:"created_at"`
+	// UpdatedAt  time.Time `json:"updated_at"`
+	// DeletedAt  time.Time `json:"deleted_at"`
+	ProfileUrl string `json:"profile_url"`
+	Bio        string `json:"bio"`
+	CoverUrl   string `json:"cover_url"`
+	IsCreator  bool   `json:"is_creator"`
 }
 
 // TableName gives table name of model
@@ -27,9 +23,9 @@ func (m User) ToMap() map[string]interface{} {
 	return map[string]interface{}{
 		"email":       m.Email,
 		"full_name":   m.FullName,
-		"created_at":  m.CreatedAt,
-		"updated_at":  m.UpdatedAt,
-		"deleted_at":  m.DeletedAt,
+		"created_at":  m.Base.CreatedAt,
+		"updated_at":  m.Base.UpdatedAt,
+		"deleted_at":  m.Base.DeletedAt,
 		"profile_url": m.ProfileUrl,
 		"bio":         m.Bio,
 		"cover_url":   m.CoverUrl,
