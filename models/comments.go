8e521e5d0767c04f0949cf877defa968ce5bfb86
Sawal Timsina <t.sawal231@gmail.com>
2022-08-29 15:51:56 +0545

    fix: comment migration and modal

--- models/comments.go
+++ models/comments.go
@@ -1,10 +1,14 @@
 package models
 
 type Comment struct {
-	CommentId  int64  `json:"comment_id"`
+	Base
 	Comment    string `json:"comment"`
 	PostId     int64  `json:"post_id"`
 	Likes      int    `json:"likes"`
 	ParentIdFk int64  `json:"parent_id_fk"`
 	UserId     int64  `json:"user_id"`
+}
+
+func (c Comment) TableName() string {
+	return "comments"
 }
