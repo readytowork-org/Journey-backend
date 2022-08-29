6247666a4beb74f0c9780f5a60325ab615a41053
saurav <saurav.maharjan7@gmail.com>
2022-08-29 15:10:16 +0545

    added PostContents field

--- models/post.go
+++ models/post.go
@@ -8,10 +8,11 @@ const (
 
 type Posts struct {
 	Base
-	PostId   int64  `json:"post_id"`
-	Title    string `json:"title"`
-	Caption  string `json:"caption"`
-	UserId   int64  `json:"user_id"`
-	Likes    int    `json:"likes"`
-	Audience string `json:"audience"`
+	PostId       int64          `json:"post_id"`
+	Title        string         `json:"title"`
+	Caption      string         `json:"caption"`
+	UserId       int64          `json:"user_id"`
+	Likes        int            `json:"likes"`
+	Audience     string         `json:"audience"`
+	PostContents []PostContents `json:"posts_contents"`
 }
