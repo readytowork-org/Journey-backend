d5cbb9a5260871e203cbb6e267b7537b38f3a0d1
damodar <damodar@varosatech.com>
2022-09-01 12:41:51 +0545

    fix: user type change in models

--- models/post.go
+++ models/post.go
@@ -10,7 +10,7 @@ type Post struct {
 	Base
 	Title        string         `json:"title"`
 	Caption      *string        `json:"caption"`
-	UserId       int64          `json:"user_id"`
+	UserId       string         `json:"user_id"`
 	Likes        int            `json:"likes"`
 	Audience     string         `json:"audience"`
 	PostContents []PostContents `json:"posts_contents"`
--- models/users.go
+++ models/users.go
@@ -1,13 +1,22 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type User struct {
-	Base
-	Email      string  `json:"email"`
-	FullName   string  `json:"full_name"`
-	ProfileUrl *string `json:"profile_url"`
-	Bio        *string `json:"bio"`
-	CoverUrl   *string `json:"cover_url"`
-	IsCreator  bool    `json:"is_creator"`
+	ID         string         `json:"id"`
+	Email      string         `json:"email"`
+	FullName   string         `json:"full_name"`
+	ProfileUrl *string        `json:"profile_url"`
+	Bio        *string        `json:"bio"`
+	CoverUrl   *string        `json:"cover_url"`
+	IsCreator  bool           `json:"is_creator"`
+	CreatedAt  time.Time      `json:"created_at"`
+	UpdatedAt  time.Time      `json:"updated_at"`
+	DeletedAt  gorm.DeletedAt `gorm:"index" json:"deleted_at"` //add soft delete in gorm
 }
 
 // TableName gives table name of model
@@ -20,9 +29,9 @@ func (m User) ToMap() map[string]interface{} {
 	return map[string]interface{}{
 		"email":       m.Email,
 		"full_name":   m.FullName,
-		"created_at":  m.Base.CreatedAt,
-		"updated_at":  m.Base.UpdatedAt,
-		"deleted_at":  m.Base.DeletedAt,
+		"created_at":  m.CreatedAt,
+		"updated_at":  m.UpdatedAt,
+		"deleted_at":  m.DeletedAt,
 		"profile_url": m.ProfileUrl,
 		"bio":         m.Bio,
 		"cover_url":   m.CoverUrl,
