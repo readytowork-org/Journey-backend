155a633b8d4f2c4fee313767c92ae68969fcf6d1
Aashish-Dahal <ashishdahal490@gmail.com>
2022-09-02 15:56:37 +0545

    fix: hot fix post like constraint

--- migration/000006_post_likes_table.up.sql
+++ migration/000006_post_likes_table.up.sql
@@ -6,12 +6,12 @@ CREATE TABLE IF NOT EXISTS `post_likes`
     PRIMARY KEY (`post_id`, `user_id`),
     INDEX `post_id_fk_idx` (`post_id` ASC) VISIBLE,
     INDEX `user_id_fk_idx` (`user_id` ASC) VISIBLE,
-    CONSTRAINT `post_id_fk`
+    CONSTRAINT `pl_post_id_fk`
         FOREIGN KEY (`post_id`)
             REFERENCES `posts` (`id`)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT `likes_user_id_fk`
+    CONSTRAINT `pl_likes_user_id_fk`
         FOREIGN KEY (`user_id`)
             REFERENCES `users` (`id`)
             ON DELETE NO ACTION
