5a442818291b60ab91a0d5b882eecbd6731ce65d
damodar <damodar@varosatech.com>
2022-09-02 16:02:04 +0545

    add: follower routes

---
+++ api/routes/followers.go
@@ -0,0 +1,48 @@
+package routes
+
+import (
+	"boilerplate-api/api/controllers"
+	"boilerplate-api/api/middlewares"
+	"boilerplate-api/infrastructure"
+)
+
+// FollowerRoutes -> struct
+type FollowerRoutes struct {
+	logger           infrastructure.Logger
+	router           infrastructure.Router
+	FollowController controllers.FollowController
+	middleware       middlewares.FirebaseAuthMiddleware
+	trxMiddleware    middlewares.DBTransactionMiddleware
+}
+
+// Setup Follower routes
+func (i FollowerRoutes) Setup() {
+	i.logger.Zap.Info(" Setting up Follower routes")
+	Posts := i.router.Gin.Group("/follow")
+	{
+		Posts.GET("/follower/:id", i.FollowController.GetFollowerCount)
+		Posts.GET("/following/:id", i.FollowController.GetFollowingCount)
+		Posts.GET("/followers/:id", i.FollowController.GetFollowers)
+		Posts.GET("/followings/:id", i.FollowController.GetFollowings)
+		Posts.POST("/", i.trxMiddleware.DBTransactionHandle(), i.FollowController.Follow)
+		Posts.DELETE("/:id", i.FollowController.UnFollow)
+
+	}
+}
+
+// NewFollowerRoutes -> creates new Post controller
+func NewFollowerRoutes(
+	logger infrastructure.Logger,
+	router infrastructure.Router,
+	FollowController controllers.FollowController,
+	middleware middlewares.FirebaseAuthMiddleware,
+	trxMiddleware middlewares.DBTransactionMiddleware,
+) FollowerRoutes {
+	return FollowerRoutes{
+		router:           router,
+		logger:           logger,
+		FollowController: FollowController,
+		middleware:       middleware,
+		trxMiddleware:    trxMiddleware,
+	}
+}
