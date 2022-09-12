e723693bd40663f43a8b31fd8da58e8f25e0e8ee
saurav <saurav.maharjan7@gmail.com>
2022-09-12 13:25:50 +0545

    added getonepost repo in updatePost, Solved issue in  PostLikes, added getonepost in deletepost

--- api/controllers/posts.go
+++ api/controllers/posts.go
@@ -8,6 +8,7 @@ import (
 	"boilerplate-api/infrastructure"
 	"boilerplate-api/models"
 	"boilerplate-api/utils"
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -66,11 +67,25 @@ func (cc PostsController) CreatePosts(c *gin.Context) {
 // UpdatePosts -> Update Post
 func (cc PostsController) UpdatePosts(c *gin.Context) {
 	trx := c.MustGet(constants.DBTransaction).(*gorm.DB)
+	id, err := strconv.Atoi(c.Param("id"))
 
 	posts := models.Post{}
+	users := models.User{}
+
+	// id, _ := strconv.ParseInt(users.ID, 10, 64)
+
 	if err := c.ShouldBindJSON(&posts); err != nil {
 		cc.logger.Zap.Error("Error [UpdatePosts] (ShouldBindJson) : ", err)
 		err := errors.BadRequest.Wrap(err, "Failed to bind Post data")
+		responses.HandleError(c, err)
+		return
+	}
+
+	_, err = cc.PostsService.GetOnePost(int64(id), users.ID)
+
+	if err != nil {
+		cc.logger.Zap.Error("Error [DeleteUser] [Conversion Error]: ", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to Parse user ID")
 		responses.HandleError(c, err)
 		return
 	}
@@ -86,8 +101,8 @@ func (cc PostsController) UpdatePosts(c *gin.Context) {
 }
 
 func (cc PostsController) PostLikes(c *gin.Context) {
-	userId := c.MustGet(constants.UID).(int64)
-	id, err := strconv.Atoi(c.Param("postId"))
+	userId := c.Query(constants.UID)
+	id, err := strconv.ParseInt(c.Param("postId"), 10, 64)
 
 	if err != nil {
 		cc.logger.Zap.Error("Error [DeletePosts] [Conversion Error]: ", err.Error())
@@ -95,6 +110,7 @@ func (cc PostsController) PostLikes(c *gin.Context) {
 		responses.HandleError(c, err)
 		return
 	}
+
 	posts, err := cc.PostsService.GetOnePost(int64(id), userId)
 
 	if err != nil {
@@ -103,6 +119,7 @@ func (cc PostsController) PostLikes(c *gin.Context) {
 		responses.HandleError(c, err)
 		return
 	}
+
 	postLike := models.PostLike{PostId: posts.ID, UserId: userId}
 	err = cc.LikesService.CreateLikes(postLike)
 	if err != nil {
@@ -111,7 +128,6 @@ func (cc PostsController) PostLikes(c *gin.Context) {
 			cc.logger.Zap.Error("Error [DeletePosts] [Conversion Error]: ", err.Error())
 			err := errors.InternalError.Wrap(err, "Failed to Parse Posts ID")
 			responses.HandleError(c, err)
-			return
 		}
 	}
 	userPostLike, err := cc.LikesService.GetUserPostLikes(postLike)
@@ -130,9 +146,19 @@ func (cc PostsController) PostLikes(c *gin.Context) {
 // DeletePosts -> Delete Posts
 func (cc PostsController) DeletePosts(c *gin.Context) {
 	id, err := strconv.Atoi(c.Param("id"))
+	users := models.User{}
 	if err != nil {
 		cc.logger.Zap.Error("Error [DeletePosts] [Conversion Error]: ", err.Error())
 		err := errors.InternalError.Wrap(err, "Failed to Parse Post ID")
+		responses.HandleError(c, err)
+		return
+	}
+
+	_, err = cc.PostsService.GetOnePost(int64(id), users.ID)
+
+	if err != nil {
+		cc.logger.Zap.Error("Error [DeleteUser] [Conversion Error]: ", err.Error())
+		err := errors.InternalError.Wrap(err, "Failed to Parse user ID")
 		responses.HandleError(c, err)
 		return
 	}
@@ -162,11 +188,14 @@ func (cc PostsController) GetAllPosts(c *gin.Context) {
 
 	responses.JSONCount(c, http.StatusOK, Posts, count)
 }
+
 // GetAllPosts -> Get All Post
 func (cc PostsController) GetCreatorPosts(c *gin.Context) {
-	userId := c.Query(constants.UID);
-  cursorPagination :=utils.BuildCursorPagination(c);
-	posts,err := cc.PostsService.CreatorPosts(cursorPagination,userId);
+	id := models.User{}
+	// userId := c.Query(constants.UID)
+	userId := c.Query(id.ID)
+	cursorPagination := utils.BuildCursorPagination(c)
+	posts, err := cc.PostsService.CreatorPosts(cursorPagination, userId)
 
 	if err != nil {
 		cc.logger.Zap.Error("Error finding Post records", err.Error())
@@ -174,13 +203,16 @@ func (cc PostsController) GetCreatorPosts(c *gin.Context) {
 		responses.HandleError(c, err)
 		return
 	}
-	responses.JSON(c, http.StatusOK, posts);
+	responses.JSON(c, http.StatusOK, posts)
 }
 
+// Get User Feeds
 func (cc PostsController) GetUserFeeds(c *gin.Context) {
-		userId := c.Query(constants.UID);
-  cursorPagination :=utils.BuildCursorPagination(c);
-	posts,err := cc.PostsService.GetUserFeeds(cursorPagination,userId);
+	id := models.User{}
+	// userId := c.Query(constants.UID)
+	userId := c.Query(id.ID)
+	cursorPagination := utils.BuildCursorPagination(c)
+	posts, err := cc.PostsService.GetUserFeeds(cursorPagination, userId)
 
 	if err != nil {
 		cc.logger.Zap.Error("Error finding Post records", err.Error())
@@ -188,14 +220,16 @@ func (cc PostsController) GetUserFeeds(c *gin.Context) {
 		responses.HandleError(c, err)
 		return
 	}
-	
 
-	responses.JSON(c, http.StatusOK, posts);
+	responses.JSON(c, http.StatusOK, posts)
 }
-func (cc PostsController) GetOnePost(c *gin.Context) {
-	userId := c.MustGet(constants.UID).(int64)
 
-	id, err := strconv.Atoi(c.Param("postId"))
+// Get one post
+func (cc PostsController) GetOnePost(c *gin.Context) {
+	users := models.User{}
+	// userId := c.MustGet(constants.UID).(int64)
+
+	id, err := strconv.Atoi(c.Param("id"))
 
 	if err != nil {
 		cc.logger.Zap.Error("Error [DeletePosts] [Conversion Error]: ", err.Error())
@@ -203,7 +237,7 @@ func (cc PostsController) GetOnePost(c *gin.Context) {
 		responses.HandleError(c, err)
 		return
 	}
-	posts, err := cc.PostsService.GetOnePost(int64(id), userId)
+	posts, err := cc.PostsService.GetOnePost(int64(id), users.ID)
 
 	if err != nil {
 		cc.logger.Zap.Error("Error [DeletePosts] [Conversion Error]: ", err.Error())
