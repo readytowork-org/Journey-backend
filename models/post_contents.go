3a807b31576e1fbc69ca35a7ba98141f6e7d90bf
damodar <damodar@varosatech.com>
2022-09-21 10:13:05 +0545

    fix: remove post feild from post contents

--- models/post_contents.go
+++ models/post_contents.go
@@ -4,7 +4,6 @@ type PostContents struct {
 	Id         int64  `json:"id"`
 	ContentUrl string `json:"content_url"`
 	PostId     int64  `json:"post_id"`
-	Post       Post   `json:"posts"`
 }
 
 func (m PostContents) TableName() string {
