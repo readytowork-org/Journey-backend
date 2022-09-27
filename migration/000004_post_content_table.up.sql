2e121a24b0f81f8c47409e04f1f7e080fe41e5db
damodar <damodar@varosatech.com>
2022-09-27 22:30:50 +0545

    fix: add content type and thumbnail column in post contents table

--- migration/000004_post_content_table.up.sql
+++ migration/000004_post_content_table.up.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS post_contents
 (
     id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
     content_url VARCHAR(500) NOT NULL,
+    content_type VARCHAR(500) ,
+    thumbnail VARCHAR(500) ,
     post_id     INT UNSIGNED NOT NULL,
     PRIMARY KEY (id),
     INDEX post_id_fk_idx (post_id ASC) VISIBLE,
